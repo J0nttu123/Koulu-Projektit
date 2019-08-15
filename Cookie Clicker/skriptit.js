21eb1d175cfbdca3ab81f1303e174f18712c0a72
Jonne Siiskonen <jonne.siiskonen@itvelhot.local>
2019-08-15 10:06:16 +0300

    valmis

--- Cookie Clicker/skriptit.js
+++ Cookie Clicker/skriptit.js
@@ -1,35 +1,63 @@
 var kerroin = 1;
 var laskuri = 0;
+var info = document.getElementById('info');
 
+function nappi(){
+    laskuri = laskuri + 1 * kerroin;
+    console.log('Pisteet: ' + laskuri);
+    tulosta();
+    info.innerHTML = "";
+}
 
-    function nappi(){
-        laskuri = laskuri + 1 * kerroin;
-        console.log('Pisteet: ' + laskuri);
-        tulosta();
-        }
+function tulosta(){
+    console.log(laskuri);
+    document.getElementById("laskenta").innerHTML = laskuri;
+}
 
-    function tulosta(){
-        console.log(laskuri);
-        document.getElementById("laskenta").innerHTML = laskuri;
-        }
-    
-    function laskuri(){
-        console.log(pisteet);
-        tulosta();
-        }
-    
-    function laske(){
-        console.log(laskuri);
-        laskuri = laskuri + 1 * kerroin * 10;
-        laskenta.innerHTML = laskuri;
-        }
+function laskuri(){
+    console.log(pisteet);
+    tulosta();
+}
 
-    function kaksi(){
-        if(laskuri < 100){
-            show();
-            info.innerHTML = "Sinulla pitää olla 100 pistettä";
-            hide();
-            return;
-        }
-        
+function laske(){
+    console.log(laskuri);
+    laskuri = laskuri + 1 * kerroin * 10;
+    laskenta.innerHTML = laskuri;
+}
+
+function kaksi(){
+    if(laskuri < 200){
+        console.log(info);
+        info.innerHTML = "Pisteet ei riitä";
+        return;
     }
+    laskuri = laskuri-200;
+    kerroin = kerroin + 1;
+    tulosta();
+    console.log("kerroin:" + kerroin);
+}
+
+function nelja(){
+    if(laskuri < 500){
+        console.log(info);
+        info.innerHTML = "Pisteet ei riitä";
+        return;
+    }
+    laskuri = laskuri-500;
+    kerroin = kerroin + 3;
+    tulosta();
+    console.log("kerroin:" + kerroin);
+}
+
+function kymmenen(){
+    if(laskuri < 2000){
+        console.log(info);
+        info.innerHTML = "Pisteet ei riitä";
+        return;
+    }
+    laskuri = laskuri-2000;
+    kerroin = kerroin + 9;
+    tulosta();
+    console.log("kerroin:" + kerroin);
+}
+
