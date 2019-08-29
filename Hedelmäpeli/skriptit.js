cf78ab31ab3c1f8a7cd5445701c0a2953a89999d
Jonne Siiskonen <jonne.siiskonen@itvelhot.local>
2019-08-29 10:28:33 +0300

    Muutoksia

--- Hedelmäpeli/skriptit.js
+++ Hedelmäpeli/skriptit.js
@@ -1,32 +1,34 @@
 var rahat = 0;
-var raha = document.getElementById('raha');
+var raha = document.getElementById('info');
 
-
-function slot1() {
+function slot() {
     Math.floor(Math.random() * 3);
 }
 
-function slot2() {
-    Math.floor(Math.random() * 3);
-}
-
-function slot3() {
-    Math.floor(Math.random() * 3);
-}
-
-function voitto() {
-    document.getElementById();
+function voitto(slot1, slot2, slot3) {
     if (slot1 == slot2 && slot2 == slot3);
     rahat = rahat + 5;
-    raha.innerHTML = "10"
     return;
 }
 
 function pelaa() {
-    slot1();
-    slot2();
-    slot3();
+
+    var n1 = slot();
+    var n2 = slot();
+    var n3 = slot();
+
+    document.getElementById("slot1").innerHTML = slot1;
+    document.getElementById("slot2").innerHTML = slot2;
+    document.getElementById("slot3").innerHTML = slot3;
+    document.getElementById("rahat").innerHTML = voitto(n1, n2, n3);
+
+    // slot1();
+    // slot2();
+    // slot3();
 }
+
+
+
 
 
 
