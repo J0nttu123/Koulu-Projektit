6bcf0349f984e38d27930c5c58adf91728a861f1
Jonne Siiskonen <jonne.siiskonen@itvelhot.local>
2019-10-04 11:31:26 +0300

    Muutoksia

--- Sikanoppapeli/skriptit.js
+++ Sikanoppapeli/skriptit.js
@@ -1,26 +1,42 @@
 var pisteet = 0;
-var pelaaja1 = 0;
-var pelaaja2 = 0;
+var pistetaulukko = [0,0];
+var vuoro = 0;
 var images = [
     "noppa1.jpg",
     "noppa2.jpg",
     "noppa3.jpg",
-    "noppa4.jpg",
-    "noppa5.jpg",
-    "noppa6.jpg"];
+    "noppa4.png",
+    "noppa5.png",
+    "noppa6.png"];
 
 function pelaa(){
     let dice = Math.floor(Math.random() * 6) + 1;
-    document.getElementById("dice").innerHTML = dice;
+    document.getElementById("noppa").src = "img/"+images[dice - 1];
     pisteet += dice; 
     document.getElementById("pisteet").innerHTML = pisteet;
-    var s1 = document.getElementById("noppa").innerHTML = pisteet
-    
-    s1.src = "img/"+images[n1];
-    s1.src = "img/"+images[n2];
-    s1.src = "img/"+images[n3];
-         
+   
+   
+      
 }
+
+function vuoronVaihto(){
+      pistetaulukko[vuoro] += pisteet;
+      vuoro = vuoro + 1;
+      if (vuoro > pistetaulukko.length - 1) {
+        vuoro = 0;
+      }
+      pisteet = 0;
+      pelaajat();
+  
+}
+
+
+function pelaajat(){
+    document.getElementById("pelaaja1").innerHTML = pistetaulukko[0];
+    document.getElementById("pelaaja2").innerHTML = pistetaulukko[1];
+
+}
+
 
 
 
